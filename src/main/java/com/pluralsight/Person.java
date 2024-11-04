0c30273cafd3d2e5959c01049a3f123e4b15a386
EdithAguirre <edithaguirre1110@gmail.com>
2024-11-04 14:27:25 -0600

    initial commit

---
+++ src/main/java/com/pluralsight/Person.java
@@ -0,0 +1,48 @@
+package com.pluralsight;
+
+public class Person implements Comparable <Person> {
+    // Data fields
+    private String firstName, lastName;
+    private int age;
+
+    // Constructor
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    // Comparator
+    @Override
+    public int compareTo(Person o) {
+        // Sort based on last name of a person
+        int last = this.lastName.compareTo(o.lastName);
+        // If same last name, then sort based on first name of person
+        return last == 0 ? this.firstName.compareTo(o.firstName) : last;
+    }
+
+    // Getters and Setters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
